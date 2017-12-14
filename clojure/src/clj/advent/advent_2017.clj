c050987f402230d663a03e34c7ff022928c01863
David Schlyter <dschlyter@gmail.com>
2017-12-14 20:34:39 +0100

    day13 + day14

--- clojure/src/clj/advent/advent_2017.clj
+++ clojure/src/clj/advent/advent_2017.clj
@@ -17,12 +17,19 @@
   (apply println args)
   (first args))
 
-(defn parse-int [s]
-      (Integer/parseInt s))
+(defn parse-int
+  ([s] (Integer/parseInt s))
+  ([s b] (Integer/parseInt s b)))
+
+(defn pad-zero [s n]
+  (apply str (take-last n (concat (repeat n \0) s))))
 
 (defn neg-range [a b]
   (reverse
     (range (inc b) (inc a))))
+
+(defn flatten-once [coll]
+  (mapcat identity coll))
 
 (defn zip [seq1 seq2]
   (map vector seq1 seq2))
@@ -43,8 +50,7 @@
 
 ; day 1
 
-; TODO read from resource file
-(def d1input "68376334795224855827459835293967497295464175589881588256882344699473595413912688278647235862566123233983921662578792917453912795352746426512649965615919588512125567186837411371179875287621488759761429629174886972298349197722423458299323141529413191327622485249495864168181327197661454464926326248274999448373741839963155646828842752761293142356422964355349521987483211496361289666375779728345952231649453711684539164893151811849653331845998998597991146881361717234517911759893792348815818755262456378627116779495435596139617246571678531183335956244163871445674244765586446362529159854137535962117184875192273872222899887357292312978286182636232921252574738118347521187637829623831872437381979223955675634257889137823684924127338433248519515211796732599314921611399736571277222546332369461136277417419794865524123989722492356536832313937597437717873787593849468836733642529378547151146397532997237439387663769334722979172954835154486382983716698212694357398153392926255272961384626131829678171219569288685597141132355322788254163923888378155573948753185423158997877718687642446457446643422536541238979761725496426292359382168535641216124211741896552562128941824172241913873537828976172738276983915232241451589421911121567228899853934667954786256223614621554618294467191255153395256524786159758429643756586457639177183891162214163549688595416893383194995824534247841414247526268212761954913719452114876764745799982792594753759626334319631191917894368116738893548797661111899664138398354818931135486984944719992393148681724116616741428937687985152658296679845474766477741553632712968679175356452987459761126437216758171182395219393289199148996813762849991484678429793578629331215796996751484375784895561682156658579887518746862371751372692472765217374791324656745291574784495299477362964676351148183676897122366838656342745944945275263617729359831466565694983217252594237828187612857523344265418227883219383138893873384775659548637662867572687198263688597865118173921615178165442133987362382721444844952715592955744739873677838847693982379696776")
+(def d1input (string/trim (slurp "input/day1.txt")))
 
 (defn day1-p1 [input]
       (->> input
@@ -61,6 +67,9 @@
            (#(let [half (/ (count %) 2)]
                (map vector % (concat (drop half %) (take half %)))))
            (filter #(apply = %)) (map first) (reduce +)))
+
+; (p (day1-p1 d1input))
+; (p (day1-p2 d1input))
 
 ; day 3 part 1
 ; the nth ring contains (n/2+1)^2 entries minus all previous entries
@@ -299,6 +308,7 @@
 (defn rotate [n list]
   (concat (drop n list) (take n list)))
 
+; TODO opt with transients?
 (defn knot [rope len]
   (let [pos (:pos rope)
         skip (:skip rope)
@@ -333,7 +343,8 @@
        (#(concat % [17, 31, 73, 47, 23]))
        (repeat 64)
        flatten
-       (#(:values (reduce knot knot-start %)))
+       (reduce knot knot-start)
+       :values
        dense-hash
        (map hexify)
        string/join))
@@ -433,3 +444,87 @@
 ; (p (count (dfs-all day12-in)))
 
 ; day 13
+
+(def day13-test (slurp "input/day13-test"))
+(def day13-in (slurp "input/day13.txt"))
+
+(defn day13-p1 [input]
+  (->> input
+       string/trim
+       string/split-lines
+       (map int-vec)
+       (filter (fn [[d r]] (= 0 (mod d (+ r (max 0 (- r 2)))))))
+       (map #(apply * %))
+       (reduce +)))
+
+(defn build-periods [input]
+  (->> input
+       string/trim
+       string/split-lines
+       (map int-vec)
+       (map #(update % 1 (fn [n] (+ n (max 0 (- n 2))))))))
+
+(defn is-caught [wait periods]
+  (some (fn [[d p]] (= 0 (mod (+ d wait) p))) periods))
+
+; (p (day13-p1 day13-test))
+; (let [periods (build-periods day13-in)]
+  ; (p (filter #(not (is-caught % periods)) (range 10000000))))
+
+; day 14
+
+(def day14-input "oundnydw")
+(def day14-test "flqrgnkx")
+
+(defn to-bin [i]
+  (pad-zero (Integer/toString i 2) 4))
+
+(defn to-bin-str [hexstr]
+  (apply str (map #(to-bin (parse-int (str %) 16)) hexstr)))
+
+(defn hashes [input]
+  (map #(knot-hash (str input "-" %)) (range 128)))
+
+(defn count-1 [strings]
+  (->> strings
+       (apply str)
+       (filter #(= \1 %))
+       count))
+
+(defn day14-p1 [input]
+  (->> input
+       hashes
+       (map to-bin-str)
+       count-1))
+
+(defn neighbours [[x y]]
+  [[(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]])
+
+(defn dfs2 [graph start visited pos]
+  (let [node (get-in graph pos)]
+    (if (and (= node \1) (not (get visited pos)))
+      (reduce #(dfs2 graph start %1 %2)
+              (assoc visited pos start)
+              (neighbours pos))
+      visited)))
+
+(defn all-positions [graph]
+  (flatten-once (map (fn [y] (map (fn [x] [y x]) (range (count (get graph y)))))
+                (range (count graph)))))
+
+(defn dfs2-all [graph]
+  (reduce #(dfs2 graph %2 %1 %2) {} (all-positions graph)))
+
+(defn regions [graph]
+  (into #{} (vals (dfs2-all graph))))
+
+(defn day14-p2 [input]
+  (->> input
+       hashes
+       (map to-bin-str)
+       (into [])
+       regions
+       count))
+
+(p (day14-p1 day14-input))
+; (p (day14-p2 day14-input))
