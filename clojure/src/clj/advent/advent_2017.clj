ad1be888e3c82f3a0015098c98b0f1cd3e18e4e0
David Schlyter <dschlyter@gmail.com>
2017-12-16 09:47:52 +0100

    day15

--- clojure/src/clj/advent/advent_2017.clj
+++ clojure/src/clj/advent/advent_2017.clj
@@ -510,7 +510,7 @@
 
 (defn all-positions [graph]
   (flatten-once (map (fn [y] (map (fn [x] [y x]) (range (count (get graph y)))))
-                (range (count graph)))))
+                 (range (count graph)))))
 
 (defn dfs2-all [graph]
   (reduce #(dfs2 graph %2 %1 %2) {} (all-positions graph)))
@@ -526,5 +526,50 @@
        regions
        count))
 
-(p (day14-p1 day14-input))
+; (p (day14-p1 day14-input))
 ; (p (day14-p2 day14-input))
+
+; day 15
+
+(def mersenne 2147483647)
+(def iterations1 40000000)
+(def iterations2 5000000)
+; (def iterations 5)
+
+(def first-16 (dec (bit-shift-left 1 16)))
+
+(def day15-test [65 8921])
+(def day15-input [512 191])
+
+(defn matches-16 [[n1 n2]]
+  (= (bit-and n1 first-16) (bit-and n2 first-16)))
+
+(defn gen-a [n]
+  (mod (* n 16807) mersenne))
+
+(defn gen-b [n]
+  (mod (* n 48271) mersenne))
+
+(defn gen-a-2 [n]
+  (let [res (mod (* n 16807) mersenne)]
+    (if (= (mod res 4) 0)
+      res
+      (recur res))))
+
+(defn gen-b-2 [n]
+  (let [res (mod (* n 48271) mersenne)]
+    (if (= (mod res 8) 0)
+      res
+      (recur res))))
+
+(defn day15-p1 [input iterations]
+  (->> (zip (iterate gen-a-2 (get input 0))
+            (iterate gen-b-2 (get input 1)))
+       (drop 1)
+       (take iterations)
+       (filter matches-16)
+       count))
+
+
+; (p (day15-p1 day15-input iterations1))
+(p (day15-p1 day15-input iterations2))
