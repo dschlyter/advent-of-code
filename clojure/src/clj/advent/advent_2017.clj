9c21fadc93eb9e70aee38e8b935950521c6d31f5
David Schlyter <dschlyter@gmail.com>
2017-12-21 16:53:48 +0100

    day 21, part 1

--- clojure/src/clj/advent/advent_2017.clj
+++ clojure/src/clj/advent/advent_2017.clj
@@ -655,3 +655,121 @@
 
 ; (p "day16 p2" (t "p2" #(day16-p2 input)))
 
+; day 17
+
+(def steps 382)
+(def start-buffer {:pos 0 :list [0]})
+
+(defn circle-insert [buf]
+  (let [list (:list buf)
+        pos (:pos buf)
+        next-pos (+ (mod (+ pos steps) (count list)) 1)]
+    (if (= 0 (mod (count list) 100000))
+      (p (count list)))
+    {:pos next-pos
+     :list (insert list next-pos (count list))}))
+
+; (p "day17 test" (take 5 (iterate circle-insert start-buffer)))
+; (p "day17 part 1" (last (take 2018 (iterate circle-insert start-buffer))))
+; (p (last (take 50000001 (iterate circle-insert start-buffer))))
+; part 2 hacked together in python
+
+; day 21
+
+
+(def day21-in (string/split-lines (slurp "input/day21.txt")))
+(def day21-test (string/split-lines (slurp "input/day21-test")))
+(def start-pattern ".#./..#/###")
+
+; how to rotate and flip strings on the format ../.#
+; 2 is /
+(def perm-2 {:rotate [3 0 2
+                      4 1]
+             :flip [3 4 2
+                    0 1]})
+; 3,7 is /
+(def perm-3 {:rotate [8 4 0 3
+                      9 5 1 7
+                      10 6 2]
+             :flip [8 9 10 3
+                    4 5 6 7
+                    0 1 2]})
+
+(defn apply-perm [str perm]
+  (string/join (map #(get str %) perm)))
+
+(defn all-perms [str perms]
+  (->> (take 4 (iterate #(apply-perm % (:rotate perms)) str))
+    (map (fn [key] [key (apply-perm key (:flip perms))]))
+    flatten))
+
+(defn all-keys [key]
+  (cond
+    (= (count key) 5) (all-perms key perm-2)
+    (= (count key) 11) (all-perms key perm-3)
+    :else (println "key len missmatch :/")))
+
+(defn make-rules [input]
+  (->> input
+        (map (fn [line] (let [parts (string/split line #" ")]
+                          [(get parts 0) (get parts 2)])))
+        (map (fn [[k v]] (for [key (all-keys k) val [v]]
+                           [key val])))
+        flatten-once
+        (into {})))
+
+; (p (doall (all-keys "12/45")))
+; (p (doall (all-keys "123/456/789")))
+; (p (make-rules day21-test))
+
+(defn grid-join [size pieces]
+  (->> pieces
+       (map #(string/split % #"/"))
+       (partition size size [])
+       (map #(apply map vector %))
+       flatten-once
+       (map #(string/join %))
+       (string/join "/")))
+
+(defn grid-split [grid size]
+  (->> grid
+       (#(string/split % #"/"))
+       (map #(partition size %))
+       (partition size)
+       (map #(apply map vector %))
+       (flatten-once)
+       (map #(map string/join %))
+       (map #(string/join "/" %))))
+
+; (p (grid-join ["12/34" "12/34" "33/44" "33/44"] 2))
+; (p (grid-split "1212/3434/3333/4444" 2))
+
+(defn enhance [grid rules]
+  (let [split-size
+        (if (= (mod (count (string/replace grid #"/" "")) 2) 0) 2 3)]
+    (->>
+      (grid-split grid split-size)
+      (map #(get rules %))
+      (grid-join split-size))))
+
+(defn count-on [grid]
+  (count (string/replace grid #"[^#]" "")))
+
+(defn day21-p1 [input]
+  (let [rules (make-rules input)]
+    (->> start-pattern
+      (iterate #(enhance % rules))
+      (take 6)
+      last
+      count-on)))
+
+(defn day21-p2 [input]
+  (let [rules (make-rules input)]
+    (->> start-pattern
+         (iterate #(enhance % rules))
+         (take 19)
+         last
+         count-on)))
+
+(p (day21-p1 day21-in))
+(p (day21-p2 day21-in))
