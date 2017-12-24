c749eb6ab3f8540dce7016111db77918332a2a77
David Schlyter <dschlyter@gmail.com>
2017-12-24 13:14:10 +0100

    day19

--- clojure/src/clj/advent/advent_2017.clj
+++ clojure/src/clj/advent/advent_2017.clj
@@ -827,6 +827,49 @@
 
 ; (p "day 23 p2" (day23-p1 day23-in))
 
+; day 19
+
+(def day19-in (string/split-lines (slurp "input/day19.txt")))
+(def day19-test (string/split-lines (slurp "input/day19-test.txt")))
+
+(def dirs
+     {:down {:add [0 1] :not-first "-" :stop "+" :turn [:right :left]}
+      :up {:add [0 (- 1)] :not-first "-" :stop "+" :turn [:right :left]}
+      :left {:add [(- 1) 0] :not-first "|" :stop "+" :turn [:up :down]}
+      :right {:add [1 0] :not-first "|" :stop "+" :turn [:up :down]}})
+
+(defn get-tube [tubes [x y]]
+  (as-> tubes t
+    (get t y)
+    (if (and (<= 0 x) (< x (count t)))
+      (subs t x (inc x))
+      " ")))
+
+(def step-count (atom 0))
+
+(defn count-steps []
+  (swap! step-count inc))
+
+(defn tube-step [tubes prev-pos dir first]
+  (let [pos (into [] (map + prev-pos (:add dir)))
+        ch (get-tube tubes pos)]
+    ; (p pos ch dir)
+    (if (re-matches #"[A-Z]" ch)
+      (print ch))
+    (if (or (= ch " ")
+            (and first (= ch (:not-first dir))))
+      pos ; done!
+      (do
+        (count-steps)
+        (if (= ch (:stop dir))
+          (doall (map #(tube-step tubes pos (get dirs %) true) (:turn dir)))
+          (recur tubes pos dir false))))))
+
+; (tube-step day19-test [5 0] (:down dirs) true)
+; (tube-step day19-in [113 0] (:down dirs) true)
+; (println)
+; (println (+ 1 @step-count))
+
 ; day 20
 
 (def day20-in
@@ -1144,5 +1187,5 @@
   (reset! size-matters true)
   (day24-p1 day24-in))
 
-(day24-p1 day24-in)
-(day24-p2 day24-in)
+; (day24-p1 day24-in)
+; (day24-p2 day24-in)
