4a807b1f3c0981aa3a16ecee50be2aa439c2dfb5
David Schlyter <dschlyter@gmail.com>
2018-12-09 12:50:43 +0100

    checkpoint 2

--- python/2018/day6.py
+++ python/2018/day6.py
@@ -1,8 +1,9 @@
 import pydash
 
 from string import ascii_lowercase
+from queue import Queue
 
-filename = 'input/day5.txt'
+filename = 'input/day6.txt'
 
 
 def main():
@@ -12,43 +13,29 @@ def main():
 
 def problem1():
     lines = read_input()
-    print("pre", len(lines[0]))
-    out = react(lines[0])
-    print(len(out))
 
+    points = []
+    for line in lines:
+        s = line.replace(",", "").split(" ")
+        print(s)
+        points.append(list(map(int, s)))
 
-def react(line):
-    reacted = True
-    curr = line
-    while reacted:
-        reacted = False
-        after = []
-        i = 0
-        while i < len(curr):
-            if not after:
-                after.append(curr[i])
-                i += 1
-            a, b = after[-1], curr[i]
-            if a != b and a.lower() == b.lower():
-                after = after[:-1]
-            else:
-                after.append(curr[i])
-            i += 1
-        curr = after
-    return curr
+    grid = [[0] * 1000] * 1000
+    print(grid)
+
+    q = Queue()
+    i = 1
+    for p in points:
+        grid[p[0]][p[1]] = i
+        q.put((p, i))
+
+    while not q.empty():
+        (p, c) = q.get()
+
 
 
 def problem2():
     lines = read_input()
-    results = []
-    for c in ascii_lowercase:
-        mod = lines[0]
-        mod = mod.replace(c, "")
-        mod = mod.replace(c.upper(), "")
-        out = react(mod)
-        results.append((c, len(out)))
-    s = sorted(results, key=lambda s: s[1])
-    print(s)
 
 
 def read_input():
--- python/2018/day7.py
+++ python/2018/day7.py
@@ -3,7 +3,7 @@ import pydash
 from string import ascii_lowercase
 from queue import Queue
 
-filename = 'input/day6.txt'
+filename = 'input/day7.txt'
 
 
 def main():
@@ -14,28 +14,35 @@ def main():
 def problem1():
     lines = read_input()
 
-    points = []
+    dep = []
     for line in lines:
-        s = line.replace(",", "").split(" ")
-        print(s)
-        points.append(list(map(int, s)))
+        s = line.split(" ")
+        dep.append([s[1], s[7]])
+    all_tasks = set([task for d in dep for task in d])
+    o = find_order(all_tasks, dep)
+    print("".join(o))
 
-    grid = [[0] * 1000] * 1000
-    print(grid)
 
-    q = Queue()
-    i = 1
-    for p in points:
-        grid[p[0]][p[1]] = i
-        q.put((p, i))
+def find_order(tasks, dep):
+    not_ready = set(map(lambda x: x[1], dep))
+    best_task = list(sorted(tasks - not_ready))[0]
 
-    while not q.empty():
-        (p, c) = q.get()
+    dep = list(filter(lambda x: x[0] != best_task, dep))
+    tasks = tasks - {best_task}
 
+    print(best_task, dep)
+    if not tasks:
+        return [best_task]
+    else:
+        return [best_task] + find_order(tasks, dep)
 
 
 def problem2():
     lines = read_input()
+
+
+def task_time(task):
+    61 + ord(task) - ord('A')
 
 
 def read_input():
