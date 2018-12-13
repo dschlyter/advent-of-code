a757d9581f73a1673fdb8e07c8f063649d11e36c
David Schlyter <dschlyter@gmail.com>
2018-12-13 10:10:24 +0100

    wip 13

--- python/2018/day13.py
+++ python/2018/day13.py
@@ -11,7 +11,7 @@ import util
 
 from multiprocessing import Pool
 
-filename = 'input/day12.txt'
+filename = 'input/day13_test.txt'
 
 
 def main():
@@ -21,60 +21,82 @@ def main():
 
 @util.timing
 def problem1():
-    pad = 40
     lines = util.read_input(filename)
-    initial, trans = parse(lines)
-    state = '.' * pad + initial + '.' * pad
-    for gen in range(20):
-        print(state)
-        newState = ".."
-        for i in range(2, len(state) - 2):
-            newState += trans.get(state[i-2:i+3], '.')
-        newState += ".."
-        state = newState
-    print(state)
+    track, carts = parse(lines)
+    show_state(track, carts)
 
-    sum = 0
-    for i in range(len(state)):
-        if state[i] == "#":
-            sum += (i-pad)
-    print(sum)
+    cnt = 0
+    while True:
+        cnt += 1
+        print(cnt)
+        carts = sorted(carts)
+        for i in range(len(carts)):
+            c = carts[i]
+            (pos, direction, count) = c
+            (y, x) = pos
+            (yd, xd) = direction
+
+            part = track[y][x]
+
+            if part ==
+
+            new_pos = (y + yd, x + xd)
+            carts[i] = (new_pos, (yd, xd), count)
+
+            for j in range(len(carts)):
+                oc = carts[j]
+                if i != j and c[0] == oc[0]:
+                    print("Collision", c[0])
+                    return
+
+
+
+def show_state(track, carts):
+    for l in track:
+        print(l)
+    print(carts)
+
+
+up = (-1, 0)
+down = (1, 0)
+left = (-1, 0)
+right = (1, 0)
+dirs = [up, down, left, right]
 
 
 def parse(lines):
-    initial = lines[0].split(" ")[2]
+    track = []
+    carts = []
 
-    trans = {'.....': '.'}
-    for l in lines[2:]:
-        s = l.split(" ")
-        trans[s[0]] = s[2]
+    y = 0
+    for l in lines:
+        x = 0
+        line = ""
+        for p in l:
+            if p == "<":
+                carts.append(((y, x), left, 0))
+                line += "-"
+            elif p == ">":
+                carts.append(((y, x), right, 0))
+                line += "-"
+            elif p == "^":
+                carts.append(((y, x), up, 0))
+                line += "|"
+            elif p == "v":
+                carts.append(((y, x), down, 0))
+                line += "|"
+            else:
+                line += p
+            x += 1
+        track.append(line)
+        y += 1
 
-    return initial, trans
+    return track, carts
 
 
 @util.timing
 def problem2():
-    pad = 1020
-    lines = util.read_input(filename)
-    initial, trans = parse(lines)
-    state = '.' * pad + initial + '.' * pad
-    for gen in range(1000):
-        print(state)
-        newState = ".."
-        for i in range(2, len(state) - 2):
-            newState += trans.get(state[i-2:i+3], '.')
-        newState += ".."
-        state = newState
-    print(state)
-
-    sum = 0
-    count = 0
-    for i in range(len(state)):
-        if state[i] == "#":
-            count += 1
-            sum += (i-pad)
-    print(sum, count)
-    print((50000000000 - 1000)*count + sum)
+    pass
 
 
 if __name__ == '__main__':
