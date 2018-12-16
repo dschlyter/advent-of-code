83ce1bc803d32d1c826b14fcd3980317137495f7
David Schlyter <dschlyter@gmail.com>
2018-12-16 15:39:55 +0100

    solve day 15 part1

--- python/2018/day15.py
+++ python/2018/day15.py
@@ -11,77 +11,168 @@ import util
 
 from multiprocessing import Pool
 
-filename = 'input/day14.txt'
+filename = 'input/day15.txt'
 
 
 def main():
-    problem1()
+    fns = ["day15_test",
+           "day15_test1",
+           "day15_test2",
+           "day15_test3",
+           "day15_test4",
+           "day15_test5",
+           "day15",
+           ]
+    # fns = [fns[0]]
+    for f in fns:
+        print(f)
+        problem1(f)
     problem2()
 
 
 @util.timing
-def problem1():
-    lines = util.read_input(filename)
-    stop = int(lines[0])
-    # stop = 2018
-    scores = [3, 7]
+def problem1(file):
+    lines = util.read_input("input/"+file+".txt")
+    terrain, units = parse_input(lines)
 
-    elf1 = 0
-    elf2 = 1
-    while len(scores) < stop + 10:
-        s = scores[elf1] + scores[elf2]
-        for d in str(s):
-            scores.append(int(d))
-        elf1 = (elf1 + scores[elf1] + 1) % len(scores)
-        elf2 = (elf2 + scores[elf2] + 1) % len(scores)
+    ticks = 0
+    for i in range(10000):
+        end = False
 
-    for s in scores[stop:stop+10]:
-        print(s, end="")
-    print()
+        player_order = pydash.sort_by(units, POS)
+        for p in player_order:
+            if p not in units:
+                continue
+
+            if len(set(map(lambda u: u[TEAM], units))) < 2:
+                end = True
+                break
+
+            move(p, terrain, units)
+
+        if end:
+            break
+
+        ticks += 1
+
+        # print(ticks)
+        # print_state(terrain, units)
+
+    print(ticks)
+    print_state(terrain, units)
+
+    health_sum = sum(map(lambda u: u[HEALTH], units))
+    print(ticks * health_sum)
+
+
+def move(p, terrain, units):
+    um = {}
+    for u in units:
+        um[u[POS]] = u
+
+    next_pos = bfs(p, terrain, um)
+    if next_pos and not um.get(next_pos):
+        p[POS] = next_pos
+
+    adj = set(nearby(p[POS]))
+    attackable = pydash.chain(units) \
+        .filter(lambda ou: (ou[POS] in adj) and (ou[TEAM] != p[TEAM])) \
+        .sort_by(lambda u: (u[HEALTH], u[POS])) \
+        .value()
+
+    if attackable:
+        target = attackable[0]
+        target[HEALTH] -= 3
+        if target[HEALTH] <= 0:
+            units.remove(target)
+
+
+def bfs(player, terrain, unit_map):
+    q = Queue()
+    visited = set()
+    solutions = []
+
+    for p in nearby(player[POS]):
+        q.put((1, p, p))
+
+    while not q.empty():
+        item = q.get()
+        (dist, pos, move) = item
+        (y, x) = pos
+        if terrain[y][x] != ".":
+            continue
+        if pos in visited:
+            continue
+        visited |= {pos}
+
+        u = unit_map.get(pos)
+        if u:
+            if u[TEAM] != player[TEAM]:
+                solutions.append(item)
+            continue
+
+        for np in nearby(pos):
+            q.put((dist+1, np, move))
+
+    if solutions:
+        return sorted(solutions)[0][2]
+
+    return None
+
+
+def nearby(pos):
+    (y, x) = pos
+    # returned in read order
+    return [(y-1, x), (y, x-1), (y, x+1), (y+1, x)]
+
+
+def parse_input(lines):
+    terrain = []
+    units = []
+
+    y = 0
+    for line in lines:
+        terrain.append([])
+        x = 0
+        for t in line:
+            if t == 'E' or t == 'G':
+                units.append({HEALTH: 200, POS: (y, x), TEAM: t, ID: len(units)})
+                t = "."
+            terrain[-1].append(t)
+            x += 1
+        y += 1
+
+    return terrain, units
+
+
+def print_state(terrain, units):
+    p = []
+    stats = {}
+    for t in terrain:
+        p.append(t.copy())
+    for u in units:
+        (y, x) = u[POS]
+        p[y][x] = u[TEAM]
+        stats[y] = stats.get(y, [])
+        stats[y].append(u)
+    y = 0
+    for t in p:
+        print("".join(t), end=" ")
+        for p in pydash.sort_by(stats.get(y, []), POS):
+            print(f"{p[TEAM]}({p[HEALTH]})", end=" ")
+        print()
+        y += 1
+
+
+HEALTH = 'health'
+POS = 'pos'
+TEAM = 'team'
+ID = 'id'
 
 
 @util.timing
 def problem2():
     lines = util.read_input(filename)
-    stop = lines[0]
-    # stop = "51589"
-    scores = [3, 7]
-
-    target = []
-    for s in stop:
-        target.append(int(s))
-    target.reverse()
-
-    elf1 = 0
-    elf2 = 1
-    m = len(stop)
-    while True:
-        s = scores[elf1] + scores[elf2]
-        for d in str(s):
-            scores.append(int(d))
-        elf1 = (elf1 + scores[elf1] + 1) % len(scores)
-        elf2 = (elf2 + scores[elf2] + 1) % len(scores)
-
-        if match(scores, len(scores) - 1, target):
-            break
-        if match(scores, len(scores) - 2, target):
-            m += 1
-            break
-
-    for s in scores[-10:]:
-        print(s, end="")
-    print()
-    print(len(scores) - m)
-
-
-def match(scores, i, target):
-    j = 0
-    while j < len(target):
-        if scores[i] != target[j]:
-            return False
-        i -= 1
-        j += 1
-    return True
 
 
 if __name__ == '__main__':
