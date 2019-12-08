73036014d12bf4aa64869ccca1f363770f005250
David Schlyter <dschlyter@gmail.com>
2019-12-08 17:20:30 +0100

    solve day 8 part 2

--- rust/2019/day8.rs
+++ rust/2019/day8.rs
@@ -30,7 +30,6 @@ fn part1(lines: &Vec<String>) {
         for y in 0..height {
             for x in 0..width {
                 let i = l * layer_size + y * width + x;
-                dbg!(l, y, x, i);
 
                 let d = digits[i];
                 if d == 0 { z += 1; }
@@ -50,7 +49,39 @@ fn part1(lines: &Vec<String>) {
 }
 
 fn part2(lines: &Vec<String>) {
+    let digits: Vec<u32> = lines[0].chars().map(|n| n.to_digit(10).unwrap()).collect();
+
+    let mut best = 999999999;
     let mut ans = 0;
+
+    let width = 25;
+    let height = 6;
+    let layer_size = width * height;
+    let layers = digits.len() / layer_size;
+
+    for y in 0..height {
+        let mut line = "".to_string();
+        for x in 0..width {
+            for l in 0..layers {
+                let i = l * layer_size + y * width + x;
+                let d = digits[i];
+
+                if d == 2 {
+                    continue;
+                }
+                
+                let s = match d {
+                    0 => " ",
+                    1 => "W",
+                    _ => panic!("")
+                };
+
+                line = format!("{}{}", line, s);
+                break;
+            }
+        }
+        println!("{}", line);
+    }
 
     println!("Part 2 answer {}", ans);
 }
