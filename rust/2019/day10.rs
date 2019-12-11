4fa6ad7d85e0857a93d65b446b91bd324087bad8
David Schlyter <dschlyter@gmail.com>
2019-12-11 19:54:48 +0100

    day 10 part 2

--- rust/2019/day10.rs
+++ rust/2019/day10.rs
@@ -1,5 +1,5 @@
 // use std::collections::HashMap;
-// use std::collections::HashSet;
+use std::collections::HashSet;
 
 mod util;
 
@@ -7,7 +7,6 @@ fn main() {
     let lines = util::get_lines();
 
     part1(&lines);
-    part2(&lines);
 }
 
 fn part1(lines: &Vec<String>) {
@@ -15,13 +14,22 @@ fn part1(lines: &Vec<String>) {
 
     // let mut dm = util::debug_map(lines.len() as i32, lines[0].len() as i32, ' ');
 
+
     let mut best = 0;
+    let mut bestPos = (0, 0);
+    let mut bestVisible = HashSet::new();
+
+
     for y in 0..lines.len() {
         for x in 0..map[y].len() {
             let mut count = 0;
+            let mut visible = HashSet::new();
+
             if map[y][x] != '#' {
                 continue;
             }
+
+
             // dbg!(format!("Checking {} {}", y, x));
             for y2 in 0..lines.len() {
                 for x2 in 0..map[y].len() {
@@ -58,9 +66,6 @@ fn part1(lines: &Vec<String>) {
                             }
 
                             if map[ypos.round() as usize][xmiddle] == '#' {
-                                if y == 1 && x == 14 {
-                                    let p: usize = ypos.round() as usize;
-                                }
                                 line_of_sight = false;
                             }
                         }
@@ -68,6 +73,7 @@ fn part1(lines: &Vec<String>) {
 
                     if line_of_sight {
                         count += 1;
+                        visible.insert((y2, x2));
                     }
                 }
             }
@@ -75,14 +81,30 @@ fn part1(lines: &Vec<String>) {
             if count > best {
                 dbg!(format!("New best {} {} {}", y, x, count));
                 best = count;
+                bestPos = (y, x);
+                bestVisible = visible;
             }
         }
     }
 
     println!("Part 1 answer {}", best);
+
+    // Note: Since answer was 299 for part 1, and part 2 asks for the 200th destroyed asteroid we do not rotate more than a full round
+
+    let (y, x) = bestPos;
+    let mut positions: Vec<(f64, usize, usize)> = bestVisible.into_iter().map(|(y2, x2)| (rot_pos(y as i32, x as i32, y2 as i32, x2 as i32), y2, x2)).collect();
+
+    positions.sort_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal));
+
+    if positions.len() >= 200 {
+        println!("Part 2 answer {}", positions[199].2 * 100 + positions[199].1);
+    }
 }
 
-fn part2(lines: &Vec<String>) {
-    let mut ans = 0;
-    println!("Part 2 answer {}", ans);
+fn rot_pos(y: i32, x: i32, y2: i32, x2: i32) -> f64 {
+    let yd = (y2 - y) as f64;
+    let xd = (x2 - x) as f64;
+    let pi = std::f64::consts::PI;
+
+    return (yd.atan2(xd) + 1e-8 + (pi/2.0) + 2.0*pi) % (2.0*pi);
 }
