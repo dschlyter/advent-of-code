652f919e6969696a609b2736a2c91ccb1cf8a334
David Schlyter <dschlyter@gmail.com>
2019-12-16 00:45:57 +0100

    solve part 2 day 15

--- rust/2019/day15.rs
+++ rust/2019/day15.rs
@@ -5,7 +5,7 @@ use std::collections::HashSet;
 use std::collections::HashMap;
 use std::collections::LinkedList;
 
-use std::{thread, time};
+use std::{thread, time, char};
 
 extern crate regex;
 
@@ -31,14 +31,14 @@ fn part1(lines: &Vec<String>) {
     let mut queue = LinkedList::new();
     let mut visited = HashSet::new();
 
-    let mut search_done = false;
     let mut steps = 0;
+    let mut oxygen_pos = (0, 0);
 
     queue.push_back((0, 0, Vec::new()));
 
     // BFS
 
-    while !search_done {
+    while queue.len() > 0 {
         let (y, x, moves) = queue.pop_front().unwrap();
 
         // dbg!((y, x, &moves));
@@ -49,9 +49,12 @@ fn part1(lines: &Vec<String>) {
 
         visited.insert((y, x));
 
+        let mut i = 0;
         for robot_move in &moves {
             c.process(*robot_move);
-            if c.read() != 1 {
+            i += 1;
+            if c.read() == 0 {
+                dbg!(i);
                 panic!("Error entering position")
             }
         }
@@ -62,12 +65,17 @@ fn part1(lines: &Vec<String>) {
 
             if res == 0 {
                 continue;
+                let new_pos = ((y + dirs[i as usize].0), (x + dirs[i as usize].1));
+                if visited.contains(&new_pos) {
+                    dbg!("tiny walls!");
+                }
             }
 
             if res == 2 {
+                dbg!((y, x));
+                oxygen_pos = (y, x);
                 if steps == 0 {
                     steps = moves.len() + 1;
-                    search_done = true;
                 }
             }
 
@@ -78,24 +86,62 @@ fn part1(lines: &Vec<String>) {
 
             // Move back by xoring with 1
             c.process(rev_dir[&i]);
-            if c.read() != 1 {
+            if c.read() == 0 {
                 panic!("Error moving back");
             }
         }
 
-        if search_done {
-            break;
-        }
-
         for robot_move in moves.iter().rev() {
             c.process(rev_dir[robot_move]);
-            if c.read() != 1 {
+            if c.read() == 0 {
                 panic!("Error returning to 0,0")
             }
         }
     }
 
     println!("Part 1 answer {}", steps);
+
+    let rooms = visited;
+
+    let mut queue2 = LinkedList::new();
+    let mut visited2 = HashSet::new();
+    let mut max_dist = 0;
+
+    queue2.push_back((oxygen_pos.0, oxygen_pos.1, 0));
+
+    let mut d = util::debug_map(45, 45, ' ');
+
+    while queue2.len() > 0 {
+        let (y, x, dist) = queue2.pop_front().unwrap();
+
+        if !rooms.contains(&(y, x)) {
+            continue;
+        }
+
+        if visited2.contains(&(y, x)) {
+            continue;
+        }
+
+        // d[(y+25) as usize][(x+25) as usize] = 'X';
+        // util::debug_map_print(&d);
+        // d[(y+25) as usize][(x+25) as usize] = char::from_digit(dist / 100 % 10, 10).unwrap();
+        // d[(oxygen_pos.0+25) as usize][(oxygen_pos.1+25) as usize] = '0';
+
+        visited2.insert((y, x));
+
+        if dist > max_dist {
+            max_dist = dist;
+        }
+
+        for i in 1..5 {
+            queue2.push_back(((y + dirs[i as usize].0), (x + dirs[i as usize].1), dist+1));
+        }
+
+    }
+
+
+    let idontknowwhy = 1;
+    println!("Part 2 answer {}", max_dist + idontknowwhy);
 }
 
 fn part2(lines: &Vec<String>) {
