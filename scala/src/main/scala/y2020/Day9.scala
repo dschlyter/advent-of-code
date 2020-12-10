b0cfdbe92861777fcb64ae11522f25ce48f80e1a
David Schlyter <dschlyter@gmail.com>
2020-12-10 20:58:16 +0100

    clean day 9

--- scala/src/main/scala/y2020/Day9.scala
+++ scala/src/main/scala/y2020/Day9.scala
@@ -12,40 +12,38 @@ object Day9 {
     // part 1
     // TODO this could be much nicer
     breakable {
-      for (i <- parsed.indices) {
-        if (i > 24) {
-          var found = false
-          for (j <- (i-25).until(i)) {
-            for (k <- (j+1).until(i)) {
-              if (parsed(j) + parsed(k) == parsed(i)) {
-                found = true
-              }
-            }
-          }
+      for (i <- 25 until parsed.size) {
+        var found = false
 
-          if (!found) {
-            ans = parsed(i)
-            println(parsed(i))
-            break
-          }
+        for (
+          j <- (i-25).until(i);
+          k <- (j+1).until(i)
+          if parsed(j) + parsed(k) == parsed(i)
+        ) {
+          found = true
+        }
+
+        if (!found) {
+          ans = parsed(i)
+          println(parsed(i))
+          break
         }
       }
     }
 
     // part 2
     breakable {
-      for (start <- parsed.indices) {
-        for (stop <- (start+2).until(parsed.size)) {
-          val slice = parsed.slice(start, stop)
+      for (
+        start <- parsed.indices;
+        stop <- (start+2).until(parsed.size)
+      ) {
+        val slice = parsed.slice(start, stop)
 
-          if (slice.sum == ans) {
-            println(slice.min + slice.max)
-            break
-          }
+        if (slice.sum == ans) {
+          println(slice.min + slice.max)
+          break
         }
       }
     }
-
-
   }
 }
