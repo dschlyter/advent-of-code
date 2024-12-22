d6dcdc4ab27c31c7904b5625d3cf5a7a1bc571c1
David Schlyter <dschlyter@gmail.com>
2024-12-22 15:46:59 +0100

    attempt at 21 - works but subtle bug produces wrong result

--- rust/2024/src/bin/day21.rs
+++ rust/2024/src/bin/day21.rs
@@ -19,28 +19,6 @@ pub fn main() {
 pub fn solve(filename: String) {
     let lines = util::read_lines(&filename);
 
-
-    // Part 1
-    let part1_answer = {
-        let mut ans = 0;
-        for code in &lines {
-            let num_part = code.replace("A", "").parse::<i32>().expect("parsed num");
-            let seq_len = path_to_seq(2, &code.to_string());
-            ans += seq_len * num_part;
-            println!("{}:{}: {:?}", file!(), line!(), (num_part, seq_len, ans));
-        }
-        ans
-    };
-    println!("{}", part1_answer);
-
-    // Part 2 - Slow brute force 24.7 seconds
-    let part2_answer = {
-        0
-    };
-    println!("{}", part2_answer);
-}
-
-pub fn path_to_seq(key_pads: usize, target: &String) -> i32 {
     let keypad: Vec<Vec<String>> = vec![
         vec!["7".to_string(), "8".to_string(), "9".to_string()],
         vec!["4".to_string(), "5".to_string(), "6".to_string()],
@@ -53,69 +31,112 @@ pub fn path_to_seq(key_pads: usize, target: &String) -> i32 {
         vec!["<".to_string(), "v".to_string(), ">".to_string()],
     ];
 
-    let mut seen = HashSet::new();
-    let mut q = VecDeque::new();
-    let s_pos_key = util::grid_find_cell(&keypad, &"A".to_string());
-    let s_pos_dir = util::grid_find_cell(&dirpad, &"A".to_string());
-
-    let mut start = Vec::new();
-    for _ in 0..key_pads {
-        start.push(s_pos_dir);
+    let bad_keys = vec![" ".to_string()].into_iter().collect::<HashSet<_>>();
+    let dirpad_keys: HashSet<String> = util::grid_mapped_by_cell(&dirpad, &bad_keys).keys().into_iter().map(|s| s.to_string()).collect();
+    // For the outer keypad the cost of any keypress is 1
+    let mut outer_cost: HashMap<(String, String), i64> = HashMap::new();
+    for key1 in &dirpad_keys {
+        for key2 in &dirpad_keys {
+            outer_cost.insert((key1.to_string(), key2.to_string()), 1);
+        }
     }
-    start.push(s_pos_key);
 
-    q.push_back((start, 1, "".to_string()));
-    // q.push_back((vec![s_pos_key], 1, "".to_string()));
+    println!("{}:{}: {:?}", file!(), line!(), (cheapest_cost_for(&dirpad, &outer_cost, &"A".to_string(), &"<".to_string())));
 
-    while let Some((state, dist, pressed)) = q.pop_front() {
-        let key = (state.clone(), dist, pressed.clone());
-        if !seen.insert(key) {
+    // Part 1
+    let part1_answer = {
+        let cost1 = cost_for_pad(&dirpad, &outer_cost);
+        let cost2 = cost_for_pad(&dirpad, &cost1);
+
+        let mut ans = 0;
+        for code in &lines {
+            let num_part = code.replace("A", "").parse::<i64>().expect("parsed num");
+            let mut press_cost = 0;
+            let mut last_key = "A".to_string();
+            for c in code.chars() {
+                let cs = c.to_string();
+                press_cost += cheapest_cost_for(&keypad, &cost2, &last_key, &cs);
+                last_key = cs;
+            }
+            ans += press_cost * num_part;
+        }
+        ans
+    };
+    println!("{}", part1_answer);
+
+    // Part 2
+    let part2_answer = {
+        let mut pad_cost = outer_cost;
+        for _ in 0..25 {
+            pad_cost = cost_for_pad(&dirpad, &pad_cost);
+        }
+
+        let mut ans = 0;
+        for code in &lines {
+            let num_part = code.replace("A", "").parse::<i64>().expect("parsed num");
+            let mut press_cost: i64 = 0;
+            let mut last_key = "A".to_string();
+            for c in code.chars() {
+                let cs = c.to_string();
+                press_cost += cheapest_cost_for(&keypad, &pad_cost, &last_key, &cs);
+                last_key = cs;
+            }
+            ans += press_cost * num_part;
+        }
+        ans
+    };
+    println!("{}", part2_answer);
+}
+
+pub fn cost_for_pad(pad: &Grid, cost: &HashMap<(String, String), i64>) -> HashMap<(String, String), i64> {
+    let bad_keys = vec![" ".to_string()].into_iter().collect::<HashSet<_>>();
+    let dirpad_keys: HashSet<String> = util::grid_mapped_by_cell(&pad, &bad_keys).keys().into_iter().map(|s| s.to_string()).collect();
+
+    let mut inner_cost: HashMap<(String, String), i64> = HashMap::new();
+    for key1 in &dirpad_keys {
+        for key2 in &dirpad_keys {
+            let cost = cheapest_cost_for(pad, cost, key1, key2);
+            inner_cost.insert((key1.to_string(), key2.to_string()), cost);
+        }
+    }
+    inner_cost
+}
+
+pub fn cheapest_cost_for(pad: &Grid, cost: &HashMap<(String, String), i64>, start: &String, target: &String) -> i64 {
+    let mut seen = HashSet::new();
+    let mut q = BinaryHeap::new();
+    let s_pos = util::grid_find_cell(&pad, start);
+
+    q.push((0, "A".to_string(), s_pos));
+
+    while let Some((neg_dist, prev_press, pos)) = q.pop() {
+        let dist = -neg_dist;
+        if !seen.insert((prev_press.to_string(), pos)) {
             continue;
         }
 
-        'outer: for press in vec!["^", "<", ">", "v", "A"] {
-            let mut key_press = press.to_string();
-            let mut new_state = vec![];
-            for i in 0..state.len() {
-                let pos = state[i];
-                let pad = match i {
-                    _ if i < state.len() - 1 => &dirpad,
-                    _ => &keypad
-                };
-                match press_key_pad(pad, pos, &key_press) {
-                    Some((p, key)) => {
-                        new_state.push(p);
-                        key_press = key;
+        for press in vec!["^", "<", ">", "v", "A"] {
+            if let Some(new_pos) = press_key_pad(pad, pos, &press.to_string()) {
+                let next_key = &pad[new_pos.0 as usize][new_pos.1 as usize];
+                let new_dist = dist + cost[&(prev_press.to_string(), press.to_string())];
+                if press == "A" {
+                    if next_key == target {
+                        return new_dist;
+                    } else {
+                        // Wrong key
+                        continue;
                     }
-                    None => continue 'outer
                 }
+                q.push((-new_dist, press.to_string(), new_pos));
             }
-
-            let mut new_pressed = pressed.to_string();
-            if key_press != "" {
-                new_pressed = new_pressed + &key_press;
-            }
-            if &new_pressed == target {
-                return dist;
-            }
-            if !target.starts_with(&new_pressed) {
-                continue;
-            }
-            q.push_back((new_state, dist + 1, new_pressed));
         }
     }
     panic!("No path found")
 }
 
-// Press a direction on a keypad
-// Return the next pos, and if applicable the key that was pressed on the keypad
-pub fn press_key_pad(pad: &Vec<Vec<String>>, pos: (i32, i32), key: &String) -> Option<((i32, i32), String)> {
-    if key == "" {
-        return Some((pos, "".to_string()));
-    } else if key == "A" {
-        let (y, x) = pos;
-        let key = &pad[y as usize][x as usize];
-        return Some((pos, key.clone()))
+pub fn press_key_pad(pad: &Vec<Vec<String>>, pos: (i32, i32), key: &String) -> Option<(i32, i32)> {
+    if key == "A" {
+        return Some(pos)
     } else {
         let mut dir_map = HashMap::new();
         dir_map.insert("^", (-1, 0));
@@ -130,6 +151,6 @@ pub fn press_key_pad(pad: &Vec<Vec<String>>, pos: (i32, i32), key: &String) -> O
         if y < 0 || x < 0 || y >= pad.len() as i32 || x >= pad[y as usize].len() as i32 || pad[y as usize][x as usize] == " " {
             return None;
         }
-        return Some((new_pos, "".to_string()));
+        return Some(new_pos);
     }
 }
